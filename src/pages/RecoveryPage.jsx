2f91c997e0c1668dcfebe06278fe4b42216a8da5
PabloVS044 <pvasquezs044@gmail.com>
2025-02-02 14:03:26 -0600

    Add password reset functionality with token handling and new Reset page

--- src/pages/RecoveryPage.jsx
+++ src/pages/RecoveryPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Swal from "sweetalert2";
 import axios from "axios";
 
@@ -18,41 +18,35 @@ const RecoveryPage = () => {
   const [step, setStep] = useState(1);
   const [email, setEmail] = useState("");
   const [code, setCode] = useState("");
-  const [generatedCode, setGeneratedCode] = useState("");
+  const [token, setToken] = useState(null);
   const [newPassword, setNewPassword] = useState("");
+  const location = useLocation();
 
   useEffect(() => {
     setIsVisible(true);
-  }, []);
+    const queryParams = new URLSearchParams(location.search);
+    const tokenFromUrl = queryParams.get("token");
+    if (tokenFromUrl) {
+      setToken(tokenFromUrl);
+      setStep(3);
+    }
+  }, [location.search]);
 
   const handleSendCode = async () => {
-    const generatedCode = Math.floor(10000000 + Math.random() * 90000000).toString();
-    setGeneratedCode(generatedCode);
-
     try {
-      await axios.post('https://backend-postgresql.vercel.app/send-code', { email, code: generatedCode });
-      Swal.fire("Código enviado", `El código ha sido enviado a ${email}`, "success");
-      setStep(2);
+      await axios.post('http://localhost:3000/api/forgotPassword', { email });
+      Swal.fire("Código enviado", `Se ha enviado un enlace a ${email} para recuperar tu contraseña.`, "success");
     } catch (error) {
       Swal.fire("Error", "No se pudo enviar el correo electrónico", "error");
     }
   };
 
-  const handleVerifyCode = () => {
-    if (code === generatedCode) {
-      setStep(3);
-    } else {
-      Swal.fire("Error", "El código ingresado es incorrecto", "error");
-    }
-  };
-
   const handleResetPassword = async () => {
     try {
-      await axios.post('https://backend-postgresql.vercel.app/reset-password', { email, newPassword });
+      await axios.post('https://backend-postgresql.vercel.app/reset-password', { token, newPassword });
       Swal.fire("Contraseña actualizada", "Tu contraseña ha sido actualizada exitosamente", "success");
       setStep(1);
       setEmail("");
-      setCode("");
       setNewPassword("");
     } catch (error) {
       Swal.fire("Error", "No se pudo actualizar la contraseña", "error");
@@ -87,7 +81,7 @@ const RecoveryPage = () => {
                 <>
                   <h2 className="text-5xl text-center font-bold text-gray-800 mb-6">Recuperar Contraseña</h2>
                   <p className="text-gray-600 text-center text-md text-gray-600 mb-6">
-                    Ingresa tu correo electrónico y te enviaremos un código para recuperar tu contraseña.
+                    Ingresa tu correo electrónico y te enviaremos un enlace para recuperar tu contraseña.
                   </p>
                   <form className="space-y-6" onSubmit={(e) => { e.preventDefault(); handleSendCode(); }}>
                     <div>
@@ -108,85 +102,12 @@ const RecoveryPage = () => {
                     </div>
                     <div>
                       <Button type="submit" className="w-full bg-base text-white hover:bg-accent rounded-md">
-                        Enviar Código
+                        Enviar Enlace
                       </Button>
                     </div>
                   </form>
                 </>
               )}
-
-              {step === 2 && (
-                <>
-                  <h2 className="text-5xl text-center font-bold text-gray-800 mb-6">Verificar Código</h2>
-                  <p className="text-gray-600 text-center text-md text-gray-600 mb-6">
-                    Ingresa el código que hemos enviado a tu correo electrónico.
-                  </p>
-                  <form className="space-y-6" onSubmit={(e) => { e.preventDefault(); handleVerifyCode(); }}>
-                    <div>
-                      <label htmlFor="code" className="block text-sm font-medium text-gray-700">Código</label>
-                      <div className="mt-1">
-                        <input
-                          id="code"
-                          name="code"
-                          type="text"
-                          required
-                          placeholder="Código de 8 dígitos"
-                          className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                          value={code}
-                          onChange={(e) => setCode(e.target.value)}
-                        />
-                      </div>
-                    </div>
-                    <div>
-                      <Button type="submit" className="w-full bg-base text-white hover:bg-accent rounded-md">
-                        Verificar Código
-                      </Button>
-                    </div>
-                  </form>
-                </>
-              )}
-
-              {step === 3 && (
-                <>
-                  <h2 className="text-5xl text-center font-bold text-gray-800 mb-6">Nueva Contraseña</h2>
-                  <p className="text-gray-600 text-center text-md text-gray-600 mb-6">
-                    Ingresa tu nueva contraseña.
-                  </p>
-                  <form className="space-y-6" onSubmit={(e) => { e.preventDefault(); handleResetPassword(); }}>
-                    <div>
-                      <label htmlFor="newPassword" className="block text-sm font-medium text-gray-700">Nueva Contraseña</label>
-                      <div className="mt-1">
-                        <input
-                          id="newPassword"
-                          name="newPassword"
-                          type="password"
-                          required
-                          placeholder="Nueva contraseña"
-                          className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                          value={newPassword}
-                          onChange={(e) => setNewPassword(e.target.value)}
-                        />
-                      </div>
-                    </div>
-                    <div>
-                      <Button type="submit" className="w-full bg-base text-white hover:bg-accent rounded-md">
-                        Actualizar Contraseña
-                      </Button>
-                    </div>
-                  </form>
-                </>
-              )}
-
-              <div className="mt-6">
-                <div className="relative">
-                  <Link
-                    to="/login"
-                    className="text-sm font-medium text-blue-600 hover:text-blue-500 flex items-center justify-center transition-colors duration-200"
-                  >
-                    ← Volver al inicio de sesión
-                  </Link>
-                </div>
-              </div>
             </div>
           </div>
         </div>
