319db0936d124dff8b98a67963a654fb875220c8
24750Montenegro <mon24750@uvg.edu.gt>
2025-03-30 16:32:25 -0600

    Se corrigio funcionalidades de crud de proyectos y se permite el asignar y desasignar usuarios a un proyecto

--- src/actions/members.js
+++ src/actions/members.js
@@ -10,9 +10,19 @@ export const getMembersByProjectIdRequest = async ({ id }) => {
       return [error, null]
     })
 }
+
+
 export const addMembersToProjectRequest = async ({ user_id, project_id }) => {
   return api
     .post("/api/members", { user_id, project_id })
-    .then(response => console.log(response) [null, response.data])
-    .catch(error => [error, null]);
+    .then((response) => [null, response.data]) // Corregido: devuelve [null, response.data]
+    .catch((error) => [error, null]);
+};
+
+
+export const deleteMemberRequest = async ({ id }) => {
+  return api
+    .delete(`/api/members/${id}`)
+    .then((response) => [null, response.data])
+    .catch((error) => [error, null]);
 };
