4b9c3efdd23f40d57b65ef8a9c35a1547794e1f0
24750Montenegro <mon24750@uvg.edu.gt>
2025-04-02 10:34:11 -0600

    Enhance modal accessibility and improve overflow handling in News and Project sections

--- src/components/junta/ProjectSection.jsx
+++ src/components/junta/ProjectSection.jsx
@@ -351,7 +351,7 @@ function ProjectsSection() {
                 />
                 <Search className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
               </div>
-              <div className="max-h-40 overflow-y-auto border rounded-lg p-4">
+              <div className="max-h-96 overflow-y-auto border rounded-lg p-4">
                 {assignedUsers.length > 0 ? (
                   assignedUsers.map((user) => (
                     <div
@@ -386,7 +386,7 @@ function ProjectsSection() {
                 />
                 <Search className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
               </div>
-              <div className="max-h-40 overflow-y-auto border rounded-lg p-4">
+              <div className="max-h-96 overflow-y-auto border rounded-lg p-4 ">
                 {availableUsers.length > 0 ? (
                   availableUsers.map((user) => (
                     <div
